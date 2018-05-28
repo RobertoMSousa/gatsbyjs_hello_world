8f01f5d0adc1fa5a8a59626b4352eb01c94cde9c
Roberto Sousa <beto.sousa22@gmail.com>
2018-05-28 03:46:09 +0100

    add navigation to header

---
+++ src/components/header.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+
+const ListLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+
+
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+      <ul style={{ listStyle: `none`, float: `right`, color: `white` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Header

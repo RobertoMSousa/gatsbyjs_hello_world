e6d1b64ea6dc41c2f333201c66a52370098f580f
Roberto Sousa <beto.sousa22@gmail.com>
2018-05-28 03:45:41 +0100

    gatsby start files

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet', {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography.js`,
+    },
+  }, `gatsby-plugin-styled-components`],
+}

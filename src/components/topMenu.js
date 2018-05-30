e5173fd9f9ed650ed4227a74dc0ac901c0d5a9ba
Roberto Sousa <beto.sousa22@gmail.com>
2018-05-30 07:12:56 +0100

    update the style of the top menu

--- src/components/topMenu.js
+++ src/components/topMenu.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 const ListLink = props =>
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li style={{ display: `inline-block`, marginRight: `1rem`, width: '150px', textAlign: 'center'}}>
     <Link to={props.to}>
       {props.children}
     </Link>
   </li>
 
 
+const TopMenuWrapper = styled.div`
+  margin: 0 auto;
+  padding: 1.45rem 1.0875rem;
+  position: relative;
+  padding-top: 20px;
+  width: 80%;
+  height: 100%;
+  display: block;
+`;
+
 const TopMenu = ({ siteTitle }) => (
-    <div
-    style={{
-      margin: '0 auto',
-      maxWidth: 960,
-      padding: '1.45rem 1.0875rem',
-    }}
-  >
-    <ul style={{ listStyle: `none`, float: `right`, color: `white` }}>
+  <TopMenuWrapper>
+    <ul style={{ color: `white`, position: 'absolute',  bottom: '0px', textAlign: 'center', listStyle: 'inside' }}>
       <ListLink to="/">Home</ListLink>
       <ListLink to="/about/">About</ListLink>
       <ListLink to="/contact/">Contact</ListLink>
       <ListLink to="/my-files/">My Files</ListLink>
       <ListLink to="/blog">Blog</ListLink>
     </ul>
-  </div>
+  </TopMenuWrapper>
 )
 
 export default TopMenu
